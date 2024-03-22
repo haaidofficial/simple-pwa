0f9a2d3c61874787e58627e2d0601410e4646272
haaidofficial <haaidrehman372@gmail.com>
2024-03-22 20:10:49 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js')
+            .then(registration => {
+                console.log('Service Worker registered:', registration);
+            })
+            .catch(error => {
+                console.error('Service Worker registration failed:', error);
+            });
+    });
+}
+
+function toggleMenu() {
+    const mobileMenu = document.querySelector('.mobile-menu');
+    
+    if (mobileMenu.style.display === "flex") {
+        mobileMenu.style.display = "none";
+    } else {
+        mobileMenu.style.display = "flex";
+    }
+}
